d0e2eb4aad6c3e7737dcaa6f53945457cf9bafd8
이지민 <68285285+jmlee119@users.noreply.github.com>
2023-02-07 17:48:43 +0900

    first commit

---
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    TextView todaydate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        todaydate =(TextView) findViewById(R.id.todayDate);
+        todaydate.setText(getTime());
+
+    }
+
+    private String getTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String getTime = dateFormat.format(date);
+        return getTime;
+    }
+}
